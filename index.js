b48e93940af889555043d2dfb25be04f8f6ff092
shravanmeena47 <shravanmeena47@gmail.com>
2021-10-30 01:49:46 +0530

    One to one adn group chat with different group

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.get("/", function (req, res) {
+  res.render("index.ejs");
+});
+
+app.get("/hello", function (req, res) {
+  res.status(200).send("I am from node");
+});
+
+io.sockets.on("connection", function (socket) {
+  socket.on("username", function (username) {
+    socket.username = username;
+    io.emit("is_online", "🔵 <i>" + socket.username + " join the chat..</i>");
+  });
+
+  socket.on("disconnect", function (username) {
+    io.emit("is_online", "🔴 <i>" + socket.username + " left the chat..</i>");
+  });
+
+  socket.on(`route`, function (data) {
+    console.log(JSON.stringify(data));
+
+    // socket.on(`chat_message ${data.room_id}`, function (data) {
+    io.emit(`chat_message ${data.room_id}`, data);
+    // });
+  });
+});
+
+const server = http.listen(500, function () {
+  console.log("Server running on port 5000");
+});
+
